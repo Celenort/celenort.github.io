0609b8dca32fe777d0f9f5c6a578c462b915b1f7
Celenort <91267199+Celenort@users.noreply.github.com>
2024-08-28 00:09:08 +0900

    Update notion-import.js

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -167,12 +167,13 @@ image:
           .catch(function (error) {
             console.log(error);
           });
+        const imgname = `${index}.png`;
 
         let res;
         if (p1 === "") res = "";
         else res = `_${p1}_`;
 
-        return `![${index++}](/${filename})${res}`; // here is the place that we must edit.
+        return `![${index++}](/${imgname})${res}`; // here is the place that we must edit.
       }
     );
 

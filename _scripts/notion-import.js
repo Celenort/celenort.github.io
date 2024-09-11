46810534233654d88d64f5bb16eca1b9ac1a8fbe
Celenort <91267199+Celenort@users.noreply.github.com>
2024-09-12 08:47:21 +0900

    Update notion-import.js

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -19,8 +19,8 @@ function escapeCodeBlock(body) {
 }
 function convertInlineEquationToBlock(body) {
   const regex = /\n^[ \t]*\$\$\n([\s\S]*?)\n[ \t]*\$\$\n/gm;
-  return body.replace(regex, function (match, leadingWhitespace, equation) {
-    return "\n"+leadingWhitespace+"{% raw %}\n"+leadingWhitespace+"$$\n" + leadingWhitespace + equation + "\n"+leadingWhitespace+"$$\n"+leadingWhitespace+"{% endraw %}\n";
+  return body.replace(regex, function (match, equation) {
+    return "\n{% raw %}\n$$\n" + equation.trim() + "\n$$\n{% endraw %}\n";
   });
 }
 

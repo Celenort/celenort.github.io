8f4242c4a2ecb0b47ad1e2a516132901d4a2708c
Celenort <91267199+Celenort@users.noreply.github.com>
2025-07-01 16:34:23 +0900

    add inlinemath, displaymath options

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -24,7 +24,9 @@ const mathjaxScript = `
         argmin: "\\\\operatorname*{arg\\,min}"
       },
       tags: "ams",
-      strict: false
+      strict: false, 
+      inlineMath: [["$", "$"], ["\\\\(", "\\\\)"]],
+      displayMath: [["$$", "$$"], ["\\\\[", "\\\\]"]]
     },
     options: {
       skipHtmlTags: ["script", "noscript", "style", "textarea", "pre"]
